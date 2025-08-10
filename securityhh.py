1ed53d328249702233f263f11cfde162eee643a5
chakalaUltra <ivanxx7777@gmail.com>
2025-08-10 14:25:09 +0300

    Update securityhh.py

--- securityhh.py
+++ securityhh.py
@@ -385,7 +385,7 @@ class PersistentVerificationView(discord.ui.View):
         
         embed = discord.Embed(
             title="🔗 Verification Link",
-            description=f"**[🔐 Click here to complete verification]({url})**\n\n.",
+            description=f"**[🔐 Click here to complete verification]({url})**\n\n",
             color=0xFFFFFF
         )
         embed.set_footer(text="🔒 Secure OAuth2 Verification")
